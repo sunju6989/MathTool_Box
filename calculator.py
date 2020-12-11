948003bf4c0acc1de914b93ad61d0bb87723fe90
sunjuCha <sunju6989@naver.com>
2020-12-11 13:25:28 +0900

    add square function

--- calculator.py
+++ calculator.py
@@ -4,9 +4,12 @@ def add(a, b):
 
 def subtract(a, b):
   return a-b
-def multiply(a, b)
-  return a*b
 
+def multiply(a, b):
+  return a*b
 
 def divide_free(a, b):
   return a/b
+
+def square(a):
+	return a*a
